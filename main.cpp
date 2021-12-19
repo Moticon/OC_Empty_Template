04db024d7b7d7d0005f1bd1b1f6098d124af3763
Pat Smith <Moticon@users.noreply.github.com>
2021-12-19 12:39:23 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include "Lab1Template1.h"
 
+/*
+ *  This is a blank template used when I don't have
+ *  any specific pre-built code for you to start with
+ *
+ *  Rename the files in this template to make sense
+ *  with the current assignment or lab exercise. 
+ *
+ * Then delete these comments, commit, and push your code.
+ */ 
+
 int main() {
 
 	return 0; 
